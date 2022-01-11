a60af2291e9d041231e4e830c9a1d932cd361bda
PeonMike <bearfrend@gmail.com>
2022-01-11 18:10:56 +0300

    first commit

---
+++ Source/ProjectVecna/ProjectVecnaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectVecnaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVECNA_API AProjectVecnaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
